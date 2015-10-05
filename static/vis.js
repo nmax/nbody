8a77f4d3f0dbcd6f8271ea29f7a1e57efc321f19
Maximilian Neger <maximilian.neger@gmail.com>
2015-10-05 19:07:21 +0200

    playing with units and stuff
    this is so mesmerizing. i can stare at this for hours -_-

--- static/vis.js
+++ static/vis.js
@@ -1,42 +1,75 @@
+/*global nbody*/
+
 var sim = nbody();
-sim.timer.dt = 0.5 * sim.timer.units.SECOND;
+sim.timer.dt = 12 * sim.timer.units.SECOND;
 
 // assuming b1 as barycenter since:
 // b1.mass >> b2.mass and b1.mass >> b3.mass
+//
+
+var globalTime = document.getElementById('global-time');
+var balls = [];
+var center = [window.innerWidth/2, window.innerHeight/2, 0];
+
+var b1pos = center;
+var b2pos = [1030, 350, 0];
+
+var virutaMass = 500e3;
+var barycenter = [( b2pos[0] + b1pos[0] ) / 2,
+                   ( b2pos[1] + b1pos[1] ) / 2,
+                   ( b2pos[2] + b1pos[2] ) / 2];
 
 var b1 = sim.bodies.createBody({
-  position: [350, 350, 0],
-  mass: 1e6,
+  //position: [350, 350, 0],
+  position: b1pos,
+  mass: 250e3,
   radius: 50,
-  isStatic: true
+  isStatic: true,
+  //startCircular: {
+    //center: barycenter,
+    //mass: virutaMass
+  //}
 });
 
 var b2 = sim.bodies.createBody({
-  position: [500, 150, 0],
-  mass: 3e2,
-  radius: 30,
-  startCircular: {
-    center: [350, 350, 0],
-    mass: b1.mass
-  }
-});
-
-var b3 = sim.bodies.createBody({
-  position: [170, 350, 0],
+  position: b2pos,
   mass: 250e3,
-  radius: 30,
-  startCircular: {
-    center: [350, 350, 0],
-    mass: b1.mass
-  }
+  radius: 50,
+  isStatic: true,
+  //startCircular: {
+    //center: barycenter,
+    //mass: virutaMass
+  //}
 });
 
+balls.push([document.getElementById('ball1'), b1]);
+balls.push([document.getElementById('ball2'), b2]);
 
-var ball1 = document.getElementById('ball1');
-var ball2 = document.getElementById('ball2');
-var ball3 = document.getElementById('ball3');
-var globalTime = document.getElementById('global-time');
+function createBody () {
+  var id = Math.round(Date.now() + (Math.random() * 1000));
+  //var flip = Math.random() > 0.5 ? b1 : b2;
+  var amplidtude = (Math.random() * 50) + 240;
+  var freq = Math.random();
+  var x = Math.cos(2 * Math.PI * freq) * amplidtude;
+  var y = Math.sin(2 * Math.PI * freq) * amplidtude;
 
+  var props = {
+    position: [barycenter[0] + x, barycenter[1] + y , 0],
+    mass: 1,
+    radius: 5,
+    startCircular: {
+      center: barycenter,
+      mass: virutaMass
+    }
+  };
+
+  var ball = sim.bodies.createBody(props);
+  var elem = document.createElement('div');
+  elem.classList.add('ball');
+  elem.setAttribute('id', id);
+  document.body.appendChild(elem);
+  balls.push([elem, ball]);
+}
 
 var posStyle = function (pos, w) {
   return 'transform: translate(' + (Math.round(pos[0] - w)) + 'px, ' + (Math.round(pos[1] - w)) + 'px);';
@@ -47,8 +80,19 @@ var readableTime = function (millies) {
   var time = new Date(startOfSim + millies);
   var rough = [time.getDate(), time.getMonth() + 1, time.getFullYear()].join('.');
   var precise = [time.getHours(), time.getMinutes(), time.getSeconds()].join(':');
-  return rough + ' - ' + precise + ' (' + ~~(millies/(1000*60)) + 'min)';
+  return rough + ' - ' + precise + ' (+'+ (millies) + 'ms)';
 };
+
+for (var i = 0; i < 40; i+=1) {
+  createBody();
+}
+
+window.addEventListener('keyup', function (e) {
+  e.preventDefault();
+  if (e.which === 32) {
+    createBody();
+  }
+}, false);
 
 var lastTick = startOfSim;
 var tick = function () {
@@ -61,9 +105,11 @@ var tick = function () {
 
   sim.timer.advance(dt * (elapsed/1000));
 
-  ball1.setAttribute('style', posStyle(b1.state.x, 50));
-  ball2.setAttribute('style', posStyle(b2.state.x, 30));
-  ball3.setAttribute('style', posStyle(b3.state.x, 25));
+  balls.forEach(function (tuple) {
+    var elem = tuple[0];
+    var ball = tuple[1];
+    elem.setAttribute('style', posStyle(ball.state.x, 5));
+  });
 
   sim.step(time, dt * (elapsed/1000));
   globalTime.innerHTML = readableTime(time);
